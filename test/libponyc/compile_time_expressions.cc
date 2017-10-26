ec9179251897c5a7c8f13bee12512012af35b64d
Luke Cheeseman <lc3912@ic.ac.uk>
2017-10-26 21:48:25 +0100

    Basic implementation of type checking compile-time expressions.
    - Moving compile-time expression tests to the correct subdirectory.
    - Utilising using the real built-in package.
    - Disabling all but the most basic of tests.
    - Adding a basic implementation of expr_constant that assigns the type of the
      compile-time expression to that of the inner expression.
    - Adding rule for the tree checker.

--- test/libponyc/compile_time_expressions.cc
+++ test/libponyc/compile_time_expressions.cc
@@ -3,12 +3,14 @@
 #include <type/subtype.h>
 #include "util.h"
 
-#define TEST_EVALUATE_COMPILE(src) DO(test_compile(src, "evaluate"))
-#define TEST_TYPE_COMPILE(src) DO(test_compile(src, "expr"))
-#define TEST_ERROR(src) DO(test_error(src, "evaluate"))
+#define TEST_COMPILE(src) DO(test_compile(src, "expr"))
+#define TEST_ERROR(src) DO(test_error(src, "expr"))
 
 class CompileTimeExpressionTest: public PassTest
 {};
+
+// TODO: in a simlar way to other tests, design the functionality
+// such that we can unit tests the compile-time expression evaluation
 
 TEST_F(CompileTimeExpressionTest, CompileTimeLiteral)
 {
@@ -17,7 +19,7 @@ TEST_F(CompileTimeExpressionTest, CompileTimeLiteral)
     "  new create(env: Env) =>\n"
     "    let x: Bool = # true";
 
-  TEST_EVALUATE_COMPILE(src);
+  TEST_COMPILE(src);
 }
 
 TEST_F(CompileTimeExpressionTest, CompileTimeLiteralTypeInference)
@@ -27,19 +29,8 @@ TEST_F(CompileTimeExpressionTest, CompileTimeLiteralTypeInference)
     "  new create(env: Env) =>\n"
     "    let x: U32 = # 3";
 
-  TEST_EVALUATE_COMPILE(src);
+  TEST_COMPILE(src);
 }
-
-TEST_F(CompileTimeExpressionTest, CompileTimeSequenceOfLiterals)
-{
-  const char* src =
-    "actor Main\n"
-    "  new create(env: Env) =>\n"
-    "    let x: Bool = # (true; false; true)";
-
-  TEST_TYPE_COMPILE(src);
-}
-
 
 TEST_F(CompileTimeExpressionTest, CompileTimeExpression)
 {
@@ -49,49 +40,26 @@ TEST_F(CompileTimeExpressionTest, CompileTimeExpression)
     "    let x: Bool = # (true or false)";
 
   set_builtin(NULL);
-  TEST_TYPE_COMPILE(src);
+  TEST_COMPILE(src);
 }
 
-TEST_F(CompileTimeExpressionTest, CompileTimeVariable)
+TEST_F(CompileTimeExpressionTest, DISABLED_CompileTimeVariable)
 {
   const char* src =
     "actor Main\n"
     "  new create(env: Env) =>\n"
     "    let x: U32 = # (let y: U32 = 3; y)";
 
-  TEST_TYPE_COMPILE(src);
+  TEST_COMPILE(src);
 }
 
-TEST_F(CompileTimeExpressionTest, CompileTimeVariableScope)
+TEST_F(CompileTimeExpressionTest, DISABLED_CompileTimeVariableScope)
 {
   const char* src =
     "actor Main\n"
     "  new create(env: Env) =>\n"
     "    let x: U32 = # (let y: U32 = 3; y)\n"
     "    let z: U32 = y";
-
-  TEST_ERROR(src);
-}
-
-TEST_F(CompileTimeExpressionTest, CompileTimeExpressionAssignedToLetAndUsed)
-{
-  const char* src =
-    "actor Main\n"
-    "  new create(env: Env) =>\n"
-    "    let x: U32 = # 2\n"
-    "    let y: U32 = # x";
-
-  TEST_EVALUATE_COMPILE(src);
-}
-
-TEST_F(CompileTimeExpressionTest, CompileTimeExpressionAssignedToVarAndUsed)
-{
-  const char* src =
-    "actor Main\n"
-    "  new create(env: Env) =>\n"
-    "    let x: U32 = # 2\n"
-    "    var y: U32 = # x\n"
-    "    let z: U32 = # y";
 
   TEST_ERROR(src);
 }
