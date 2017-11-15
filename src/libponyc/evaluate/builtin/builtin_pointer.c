c71748ed960d22948d7dd3217c618fd602c79605
Luke Cheeseman <luke_cheeseman@hotmail.co.uk>
2017-11-15 22:37:42 +0000

    Lots added in this commit
    - Evaluated args for builtins are now passed as an array instead of as the
      sub-children of a positional args node, this is because the ast_append call
      can (and will) duplicate the node. The duplication means that var field
      reassignment doesn't work as you change the value on a duplicated node.
    - Literal values constructors don't construct an object (this is still up for
      debate as to whethere they should have a wrapper around the literal or not)
    - Literals expressions evaluate to a duplicate of the node, the original node
      may get freed after replacing the tree and breaking the definition of a
      compile-time object.
    - The symbol table doesn't return or add a duplicated node, we really don't want
      duplicates of values being returned to us as field reassignment becomes
      incredibly broken.
    - Adding a dummy implementation of clz for integers, really we want this to be
      aware of the target data sizes for USize/ULong, this is going to take some
      fiddling in the backend.
    - Adding tests for var fields in compile-time objects.
    - Currently there is a known bug that i'm working on: for some reason variable
      names that clash with the names of fields constructed in a compile-time object
      cause a whole heap of assert failure.
    - Adding some builtin pointer methods and codegen for constructing the pointer.
      This means that we can now construct/append/update compile-time arrays.

---
+++ src/libponyc/evaluate/builtin/builtin_pointer.c
@@ -0,0 +1,95 @@
+#include "builtin_pointer.h"
+#include "../evaluate.h"
+#include "../../type/subtype.h"
+#include "../../../libponyrt/mem/pool.h"
+#include "ponyassert.h"
+
+typedef struct pointer {
+  uint64_t size;
+  ast_t** elements;
+} pointer_t;
+
+uint64_t ptr_get_size(pointer_t* ptr)
+{
+  return ptr->size;
+}
+
+ast_t** ptr_get_elements(pointer_t* ptr)
+{
+  return ptr->elements;
+}
+
+bool pointer_create(pass_opt_t* opt, ast_t* receiver, ast_t** args,
+                    ast_t** result)
+{
+  (void) args;
+  if(!construct_object(opt, receiver, result))
+    return false;
+
+  pointer_t* structure = POOL_ALLOC(pointer_t);
+  structure->elements = NULL;
+  structure->size = 0;
+  ast_setdata(*result, structure);
+  return true;
+}
+
+bool pointer_alloc(pass_opt_t* opt, ast_t* receiver, ast_t** args,
+                   ast_t** result)
+{
+  if(!construct_object(opt, receiver, result))
+    return false;
+
+  lexint_t* size = ast_int(args[0]);
+  ast_t** elements = ponyint_pool_alloc_size(sizeof(ast_t*) * size->low);
+  for(uint64_t i = 0; i < size->low; ++i)
+    elements[i] = NULL;
+
+  pointer_t* structure = POOL_ALLOC(pointer_t);
+  structure->elements = elements;
+  structure->size = size->low;
+
+  ast_setdata(*result, structure);
+  return true;
+}
+
+bool pointer_realloc(pass_opt_t* opt, ast_t* receiver, ast_t** args,
+                     ast_t** result)
+{
+  (void) opt;
+  pony_assert(is_pointer(ast_type(receiver)));
+  pointer_t* structure = ast_data(receiver);
+
+  lexint_t* size = ast_int(args[0]);
+  ast_t** elements = ponyint_pool_realloc_size(sizeof(ast_t*) * structure->size,
+                                               sizeof(ast_t*) * size->low,
+                                               structure->elements);
+  structure->elements = elements;
+  structure->size = size->low;
+  *result = receiver;
+  return true;
+}
+
+bool pointer_apply(pass_opt_t* opt, ast_t* receiver, ast_t** args,
+                   ast_t** result)
+{
+  (void) opt;
+  pony_assert(is_pointer(ast_type(receiver)));
+  pointer_t* structure = ast_data(receiver);
+  uint64_t index = ast_int(args[0])->low;
+  pony_assert(index < structure->size);
+  *result = structure->elements[index];
+  return true;
+}
+
+bool pointer_update(pass_opt_t* opt, ast_t* receiver, ast_t** args,
+                   ast_t** result)
+{
+  (void) opt;
+  pony_assert(is_pointer(ast_type(receiver)));
+  pointer_t* structure = ast_data(receiver);
+  uint64_t index = ast_int(args[0])->low;
+  pony_assert(index < structure->size);
+  *result = structure->elements[index];
+  structure->elements[index] = args[1];
+  return true;
+}
---
+++ src/libponyc/evaluate/builtin/builtin_pointer.h
@@ -0,0 +1,106 @@
+#ifndef BUILTIN_POINTER_H
+#define BUILTIN_POINTER_H
+
+#include "../../ast/ast.h"
+#include "../../pass/pass.h"
+
+typedef struct pointer pointer_t;
+
+uint64_t ptr_get_size(pointer_t* ptr);
+
+ast_t** ptr_get_elements(pointer_t* ptr);
+
+bool pointer_create(pass_opt_t* opt, ast_t* receiver, ast_t** args,
+                    ast_t** result);
+bool pointer_alloc(pass_opt_t* opt, ast_t* receiver, ast_t** args,
+                   ast_t** result);
+bool pointer_realloc(pass_opt_t* opt, ast_t* receiver, ast_t** args,
+                     ast_t** result);
+bool pointer_apply(pass_opt_t* opt, ast_t* receiver, ast_t** args,
+                   ast_t** result);
+bool pointer_update(pass_opt_t* opt, ast_t* receiver, ast_t** args,
+                    ast_t** result);
+
+/*
+  fun tag _unsafe(): Pointer[A] ref =>
+    """
+    Unsafe change in reference capability.
+    """
+    compile_intrinsic
+
+  fun _offset(n: USize): this->Pointer[A] =>
+    """
+    Return a pointer to the n-th element.
+    """
+    compile_intrinsic
+
+  fun tag _element_size(): USize =>
+    """
+    Return the size of a single element in an array of type A.
+    """
+    compile_intrinsic
+
+  fun ref _insert(n: USize, len: USize): Pointer[A] =>
+    """
+    Creates space for n new elements at the head, moving following elements.
+    The array length before this should be len, and the available space should
+    be at least n + len.
+    """
+    compile_intrinsic
+
+  fun ref _delete(n: USize, len: USize): A^ =>
+    """
+    Delete n elements from the head of pointer, compact remaining elements of
+    the underlying array. The array length before this should be n + len.
+    Returns the first deleted element.
+    """
+    compile_intrinsic
+
+  fun _copy_to(that: Pointer[this->A!], n: USize): this->Pointer[A] =>
+    """
+    Copy n elements from this to that.
+    """
+    compile_intrinsic
+
+  fun ref _consume_from(that: Pointer[A]^, n: USize): Pointer[A]^ =>
+    """
+    Copy n elements from that to this.
+    """
+    compile_intrinsic
+
+  fun tag usize(): USize =>
+    """
+    Convert the pointer into an integer.
+    """
+    compile_intrinsic
+
+  fun tag is_null(): Bool =>
+    """
+    Return true for a null pointer, false for anything else.
+    """
+    compile_intrinsic
+
+  fun tag eq(that: Pointer[A] tag): Bool =>
+    """
+    Return true if this address is that address.
+    """
+    compile_intrinsic
+
+  fun tag lt(that: Pointer[A] tag): Bool =>
+    """
+    Return true if this address is less than that address.
+    """
+    compile_intrinsic
+
+  fun tag ne(that: Pointer[A] tag): Bool => not eq(that)
+  fun tag le(that: Pointer[A] tag): Bool => lt(that) or eq(that)
+  fun tag ge(that: Pointer[A] tag): Bool => not lt(that)
+  fun tag gt(that: Pointer[A] tag): Bool => not le(that)
+
+  fun tag hash(): U64 =>
+    """
+    Returns a hash of the address.
+    """
+    usize().hash()
+*/
+#endif
